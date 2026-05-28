a3b2a21b06793c8e8abc0b1650390355dd0411fa
Aurys84 <aurys84@gmail.com>
2026-05-29 01:11:41 +0200

    Initial commit - Matrix 70 Pro Localhost Launcher

---
+++ app/src/main/java/com/matrix/launcher/MainActivity.java
@@ -0,0 +1,18 @@
+package com.matrix.launcher;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        setContentView(webView);
+        webView.loadUrl("http://localhost:3000");
+    }
+}
